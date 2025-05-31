55fdb2c0c2850e4954fa0ff0b589f0cfdd1c1edc
Muhammad Said <72512584+msaidd15@users.noreply.github.com>
2025-05-31 20:39:38 +0700

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="text-4xl text-blue-100 font-bold text-center mt-10">
+      Halo bg! Tailwind sekarang aktif 🎉
+    </main>
+  );
+}
